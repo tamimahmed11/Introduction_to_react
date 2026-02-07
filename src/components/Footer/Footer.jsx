07a611ad0341716c624006a5589d3cb431cbb81c
unknown <tamim22160334.gmail.com>
2026-02-07 16:38:59 +0600

    fixed problem

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,10 @@
 
-// const Footer = () => {
-//     return (
-//         <div>
-//             <p><small>Thank  you for visiting our website</small></p>
-//         </div>
-//     );
-// };
+const Footer = () => {
+    return (
+        <div>
+            <p><small>Thank  you for visiting our website</small></p>
+        </div>
+    );
+};
+
+export default Footer;
